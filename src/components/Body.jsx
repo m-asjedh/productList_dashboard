e54ab664d8144f57afb4151dc768f22817dff391
Asjedh02 <m.asjedhcr7@gmail.com>
2024-02-05 15:50:11 +0530

    final update

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,76 +2,111 @@ import React, { useState } from "react";
 import "../index.css";
 import ReactPaginate from "react-paginate";
 import data from "../data.json";
+import { IoSearch } from "react-icons/io5";
 
 const Body = () => {
   const [products, setProducts] = useState(data.slice(0, 12));
   const [pageNumber, setPageNumber] = useState(0);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const productsPerPage = 4;
   const pagesVisited = pageNumber * productsPerPage;
 
-  const displayProducts = products
+  //Filter the search products
+  const filteredProducts = products.filter((item) =>
+    item.productName.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  //Pagination
+  const displayProducts = filteredProducts
     .slice(pagesVisited, pagesVisited + productsPerPage)
-    .map((item, index) => {
-      return (
-        <div
-          key={index}
-          className="border shadow-xl rounded-lg overflow-hidden hover:shadow-2xl transition duration-300 glassmorphism "
-        >
-          <img
-            src={item.image}
-            alt={item.productName}
-            className="w-full h-[300px] object-cover rounded-t-lg"
-          />
-          <div className="p-4">
-            <h1 className="text-xl font-semibold text-gray-800 mb-2">
-              {item.productName}
-            </h1>
-            <p className="text-lg text-gray-600 mb-2">{item.description}</p>
-            <div className="flex justify-between items-center">
-              <div>
-                <p className="text-xl font-semibold text-green-600">
-                  {item.price}
-                </p>
-                <p className="text-lg text-gray-700">
-                  Quantity: {item.quantity}
-                </p>
-              </div>
-              <div className="flex space-x-4">
-                <button className="bg-green-500 text-white rounded-full px-4 py-1 hover:bg-white hover:text-green-500 border border-green-500 transition duration-300">
-                  Edit
-                </button>
-                <button className="bg-red-500 text-white rounded-full px-4 py-1 hover:bg-white hover:text-red-500 border border-red-500 transition duration-300">
-                  View
-                </button>
-              </div>
+    .map((item, index) => (
+      <div
+        key={index}
+        className="border shadow-xl rounded-lg overflow-hidden hover:shadow-2xl transition duration-300 glassmorphism "
+      >
+        <img
+          src={item.image}
+          alt={item.productName}
+          className="w-full h-[300px] object-cover rounded-t-lg"
+        />
+        <div className="p-4">
+          <h1 className="text-xl font-semibold text-gray-800 mb-2">
+            {item.productName}
+          </h1>
+          <p className="text-lg text-gray-600 mb-2">{item.description}</p>
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="text-xl font-semibold text-green-600">
+                {item.price}
+              </p>
+              <p className="text-lg text-gray-700">Quantity: {item.quantity}</p>
+            </div>
+            <div className="flex space-x-4">
+              <button className="bg-green-500 text-white rounded-full px-4 py-1 hover:bg-white hover:text-green-500 border border-green-500 transition duration-300">
+                Edit
+              </button>
+              <button className="bg-red-500 text-white rounded-full px-4 py-1 hover:bg-white hover:text-red-500 border border-red-500 transition duration-300">
+                View
+              </button>
             </div>
           </div>
         </div>
-      );
-    });
+      </div>
+    ));
 
-  const pageCount = Math.ceil(products.length / productsPerPage);
+  const pageCount = Math.ceil(filteredProducts.length / productsPerPage);
 
   const changePage = ({ selected }) => {
     setPageNumber(selected);
   };
 
+  //Brand Filter
   const filterBrand = (category) => {
     const filteredData = data.filter((item) => item.category === category);
     setProducts(filteredData.slice(0, productsPerPage));
     setPageNumber(0);
   };
 
+  //Price Filter
   const filterPrice = (price) => {
     const filteredData = data.filter((item) => item.price === price);
     setProducts(filteredData.slice(0, productsPerPage));
     setPageNumber(0);
   };
 
+  //Search  Functionality
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+    setPageNumber(0);
+  };
+
+  //Message  for no results found
+  const renderNoResultsMessage = () => {
+    if (filteredProducts.length === 0 && searchQuery !== "") {
+      return (
+        <p className="text-center text-black mt-4">
+          No products found for "{searchQuery}".
+        </p>
+      );
+    }
+    return null;
+  };
+
   return (
     <div className="max-w-[1640px] mx-auto px-4 py-12 bg-gradient-to-b from-gray-500 via-gray-200 to-white">
-      <h1 className=" font-bold text-4xl text-center ml-2 mb-10">Products</h1>
+      <h1 className="font-bold text-4xl text-center ml-2 mb-10">Products</h1>
+      <div className="flex justify-center items-center mb-8">
+        <div className="bg-white p-4 rounded-full flex items-center w-[200px] sm:w[300px] lg:w-[400px]">
+          <IoSearch size={30} className="mr-3" />
+          <input
+            placeholder="Search Here... "
+            className="bg-transparent p-2 focus:outline-none w-full text-black"
+            value={searchQuery}
+            onChange={handleSearch}
+          />
+        </div>
+      </div>
       <div className="flex flex-col lg:flex-row justify-between">
         <div>
           <p className="font-bold text-gray-700 flex justify-center items-center m-3 text-xl">
@@ -149,9 +184,10 @@ const Body = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-2 lg:grid-cols-4  gap-6 pt-4">
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
         {displayProducts}
       </div>
+      {renderNoResultsMessage()}
       <ReactPaginate
         className="flex justify-center items-center mt-8 space-x-4 p-3 mx-auto"
         previousLabel={"Previous"}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,27 @@
 import React from "react";
-import { IoSearch } from "react-icons/io5";
 import { RxAvatar } from "react-icons/rx";
 
 const Header = () => {
   return (
-    <div className="w-full mx-auto flex justify-between items-center p-10 bg-gray-500  ">
+    <div className="w-full mx-auto flex justify-between items-center p-10 bg-gray-500">
       <div>
         <h1 className="text-lg sm:text-1xl lg:text-6xl px-3 font-extrabold font-signature cursor-pointer">
           Shoe Hub
         </h1>
       </div>
-      <div className="bg-white p-4 rounded-full flex items-center w-[200px] sm:w[300px] lg:w-[400px]">
-        <IoSearch size={30} className="mr-3" />
-        <input
-          placeholder="Search Here... "
-          className="bg-transparent p-2 focus:outline-none w-full "
-        ></input>
+      <div className="flex items-center space-x-4">
+        <nav>
+          <ul className="flex space-x-4 text-2xl gap-10">
+            <li>Home</li>
+            <li className="underline underline-offset-4">Products</li>
+            <li>Cart</li>
+            <li>About Us</li>
+          </ul>
+        </nav>
       </div>
-      <div className="flex  ">
+      <div className="flex items-center">
         <RxAvatar size={40} className="mr-2 cursor-pointer" />
-        <div className=" flex justify-center items-center text-xl font-semibold">
-          Asjedh
-        </div>
+        <div className="text-xl font-semibold">Asjedh</div>
       </div>
     </div>
   );
